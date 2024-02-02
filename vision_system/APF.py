5f4c0ca865122dbd133da5cccc96f92bfaa0a946
Daniel Glemminge <dglemminge@gmail.com>
2024-02-02 12:09:47 +0100

    APF planner concept now works, onto something

--- vision_system/APF.py
+++ vision_system/APF.py
@@ -1,33 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
+from matplotlib import patches
 
 class PotentialFieldPlanner:
-    def __init__(self, start, goal, obstacles, k_att=1, k_rep=1, rep_radius=5.0, step_size=1, max_iters=10):
+    def __init__(self, start, goal, obstacles, k_att=0.4, k_rep=4, step_size=0.5, max_iters=50):
         self.start = start
         self.goal = goal
         self.obstacles = obstacles
         self.k_att = k_att
         self.k_rep = k_rep
-        self.rep_radius = rep_radius
         self.step_size = step_size
         self.max_iters = max_iters
 
     def attractive_potential(self, position):
         theta_goal = np.arctan2(goal[1]-position[1], goal[0]-position[0])
-        attractive_potential_x = (0.5 * self.k_att * np.linalg.norm(position - self.goal))*np.cos(theta_goal)
-        attractive_potential_y = (0.5 * self.k_att * np.linalg.norm(position - self.goal))*np.sin(theta_goal)
+        # attractive_potential_x = (0.5 * self.k_att * np.linalg.norm(position - self.goal))*np.cos(theta_goal)
+        # attractive_potential_y = (0.5 * self.k_att * np.linalg.norm(position - self.goal))*np.sin(theta_goal)
+        attractive_potential_x = self.k_att * (np.linalg.norm(self.start - self.goal)/10)*np.cos(theta_goal)
+        attractive_potential_y = self.k_att * (np.linalg.norm(self.start - self.goal)/10)*np.sin(theta_goal)
+        
         return attractive_potential_x, attractive_potential_y
 
     def repulsive_potential(self, position):
         repulsive_force_x = 0
         repulsive_force_y = 0
         for obstacle in self.obstacles:
-            distance = np.linalg.norm(position - obstacle) 
-            if distance < self.rep_radius:
-                theta_position_obstacle = np.arctan2(obstacle[1]-position[1], obstacle[0]-position[0])
-                repulsive_force_x += -0.5 * (self.k_rep * ((1 / distance) - (1 / self.rep_radius)) **2)*np.cos(theta_position_obstacle)
-                repulsive_force_y += -0.5 * (self.k_rep * ((1 / distance) - (1 / self.rep_radius)) **2)*np.sin(theta_position_obstacle)
+            radius = obstacle[2]
+            distance = np.linalg.norm(position - obstacle[:2]) 
+            if distance < radius:
+                theta_obstacle = np.arctan2(obstacle[1]-position[1], obstacle[0]-position[0])
+                repulsive_force_x += self.k_rep * (radius**2/distance**2)*np.cos(theta_obstacle)
+                repulsive_force_y += self.k_rep * (radius**2/distance**2)*np.sin(theta_obstacle)
         return repulsive_force_x, repulsive_force_y
 
     def plan(self):
@@ -35,11 +39,12 @@ class PotentialFieldPlanner:
         path = [current_position]
 
         for _ in range(self.max_iters):
+
             attractive_force_x, attractive_force_y = self.attractive_potential(current_position)
             repulsive_force_x, repulsive_force_y = self.repulsive_potential(current_position)
 
-            total_force_x = repulsive_force_x + attractive_force_x
-            total_force_y = repulsive_force_y + attractive_force_y
+            total_force_x = attractive_force_x - repulsive_force_x
+            total_force_y = attractive_force_y - repulsive_force_y
 
             next_position_x = current_position[0] + self.step_size * total_force_x
             next_position_y = current_position[1] + self.step_size * total_force_y
@@ -59,8 +64,9 @@ class PotentialFieldPlanner:
 # Example usage:
 start = np.array([180, 180])
 goal = np.array([480, 360])
-# obstacles = [np.array([370, 280])]
-obstacles = []
+#obstacles = [np.array([370, 280,20])]
+obstacles = [np.array([250, 225, 20]), np.array([350, 280, 20])]
+#obstacles = [np.array([300, 230,20]), np.array([300, 250, 20]), np.array([300, 270, 20])] # wall
 
 planner = PotentialFieldPlanner(start, goal, obstacles)
 path = planner.plan()
@@ -70,6 +76,7 @@ plt.plot(start[0], start[1], 'go', label='Start')
 plt.plot(goal[0], goal[1], 'ro', label='Goal')
 for obstacle in obstacles:
     plt.plot(obstacle[0], obstacle[1], 'ks', label='Obstacle')
+    plt.gca().add_patch(patches.Circle(obstacle[:2],obstacle[2], edgecolor='g', facecolor='none'))
 plt.legend()
 plt.xlabel('X')
 plt.ylabel('Y')
