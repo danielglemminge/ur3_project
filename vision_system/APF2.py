0341da0c64a42c313d64b57d5ac68087b9eead70
Daniel Glemminge (laptop) <daniel.glemminge@nmbu.no>
2024-02-07 14:34:10 +0100

    debugging APF2

--- vision_system/APF2.py
+++ vision_system/APF2.py
@@ -5,7 +5,7 @@ from matplotlib import patches
 from numpy.linalg import norm
 
 class PotentialFieldPlanner2:
-    def __init__(self, start, goal, obstacles, k_att=0.04, k_rep=4, k_centerline=1, step_size=0.5, max_iters=50):
+    def __init__(self, start, goal, obstacles, k_att=1, k_rep=4, k_centerline=1, step_size=2, max_iters=50):
         self.start = start
         self.goal = goal
         self.obstacles = obstacles
@@ -16,40 +16,47 @@ class PotentialFieldPlanner2:
         self.max_iters = max_iters
 
     def attractive_goal(self, position):
-        theta_goal = np.arctan2(self.goal[1]-position[1], self.goal[0]-position[0]) # Angle between line from start-goal and x_axis
-        d_goal = np.linalg.norm(self.start - self.goal) # Distance to goal
-        attractive_goal_x = self.k_att * d_goal * np.cos(theta_goal)
-        attractive_goal_y = self.k_att * d_goal * np.sin(theta_goal)
+        theta_goal = np.arctan2(self.goal[1]-self.start[1], self.goal[0]-self.start[0]) # Angle between current position and goal
+        attractive_goal_x = self.k_att * np.cos(theta_goal)
+        attractive_goal_y = self.k_att * np.sin(theta_goal)
         
         return attractive_goal_x, attractive_goal_y
     
     def attractive_centerline(self, position):
-        phi_centerline = np.arctan2(self.goal[1]-position[1], self.goal[0]-position[0])
+        phi_centerline = np.arctan2(self.goal[1]-self.start[1], self.goal[0]-self.start[0])
         normal_distance_centerline = norm(np.cross(self.goal - self.start, self.start - position))/norm(self.goal - self.start)
-        # attractive_centerline_x = 0
+        # attractive_centerline_x =  0
         # attractive_centerline_y = 0
-        for obstacle in self.obstacles:
-            radius = obstacle[2]
-            dist_to_obstacle = np.linalg.norm(position - obstacle[:2])
-            if dist_to_obstacle > 1.2*radius: # Is the current point outside the obstacle?
-                attractive_centerline_x = self.k_centerline * normal_distance_centerline * np.sin(phi_centerline) 
-                attractive_centerline_y = self.k_centerline * normal_distance_centerline * np.cos(phi_centerline)
-            else: # current point is inside obstacle, so centerline attraction is ignored
-                attractive_centerline_x = 0
-                attractive_centerline_y = 0
+        if len(obstacles)>=1:
                 
+            for obstacle in self.obstacles:
+                radius = obstacle[2]
+                dist_to_obstacle = np.linalg.norm(position - obstacle[:2])
+                if dist_to_obstacle > 1.2*radius: # Is the current point outside the obstacle?
+                    attractive_centerline_x = normal_distance_centerline * np.sin(phi_centerline) 
+                    attractive_centerline_y = normal_distance_centerline * np.cos(phi_centerline)
+                else: # current point is inside obstacle, so centerline attraction is ignored
+                    attractive_centerline_x = 0
+                    attractive_centerline_y = 0
+        else:
+            attractive_centerline_x = normal_distance_centerline * np.sin(phi_centerline) 
+            attractive_centerline_y = normal_distance_centerline * np.cos(phi_centerline)
         return attractive_centerline_x, attractive_centerline_y
 
     def repulsive_obstacle(self, position):
         repulsive_obstacle_x = 0
         repulsive_obstacle_y = 0
-        for obstacle in self.obstacles:
-            radius = obstacle[2]
-            distance = np.linalg.norm(position - obstacle[:2]) 
-            if distance < radius:
-                theta_obstacle = np.arctan2(obstacle[1]-position[1], obstacle[0]-position[0])
-                repulsive_obstacle_x += self.k_rep * (radius**2/distance**2)*np.cos(theta_obstacle)
-                repulsive_obstacle_y += self.k_rep * (radius**2/distance**2)*np.sin(theta_obstacle)
+        if len(obstacles)>=1:    
+            for obstacle in self.obstacles:
+                radius = obstacle[2]
+                distance = np.linalg.norm(position - obstacle[:2]) 
+                if distance < radius:
+                    theta_obstacle = np.arctan2(obstacle[1]-position[1], obstacle[0]-position[0])
+                    repulsive_obstacle_x += self.k_rep * (radius**2/distance**2)*np.cos(theta_obstacle)
+                    repulsive_obstacle_y += self.k_rep * (radius**2/distance**2)*np.sin(theta_obstacle)
+        else:
+            repulsive_obstacle_x = 0
+            repulsive_obstacle_y = 0
         return repulsive_obstacle_x, repulsive_obstacle_y
 
     def plan(self):
@@ -87,8 +94,8 @@ if __name__=="__main__":
     start = np.array([180, 180])
     goal = np.array([480, 360])
     #obstacles = [np.array([370, 280,20])]
-    obstacles = [np.array([250, 225, 20]), np.array([350, 280, 20])]
-    #obstacles = [np.array([300, 230,20]), np.array([300, 250, 20]), np.array([300, 270, 20])] # wall
+    # obstacles = [np.array([250, 225, 20]), np.array([350, 280, 20])]
+    obstacles = [] # wall
 
     planner = PotentialFieldPlanner2(start, goal, obstacles)
     path = planner.plan()
