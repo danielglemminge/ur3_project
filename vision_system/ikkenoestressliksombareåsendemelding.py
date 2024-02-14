b3b8b7a0a42b53e3b13bf3fbcfbc64d6c0bbc89a
Daniel Glemminge (laptop) <daniel.glemminge@nmbu.no>
2024-02-14 13:53:34 +0100

    progress

--- vision_system/ikkenoestressliksombareåsendemelding.py
+++ vision_system/ikkenoestressliksombareåsendemelding.py
@@ -2,14 +2,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-
-
-k_rep = 1
+path_to_input = '/home/daniel/catkin_ws/src/ur3_project/vision_system/images_michael/'
 
 
 
-for distance in range(20,60,1):
-    repulsive_force = k_rep*((1/(distance))-(1/distance**3))*(1/(distance**3))
-    plt.plot(distance, repulsive_force)
-
-plt.show()
