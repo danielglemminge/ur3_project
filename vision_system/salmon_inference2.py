286365ef1637ab9bb0a9a5e9c673a002251f00a1
Daniel Glemminge (laptop) <dglemminge@gmail.com>
2024-03-12 15:13:17 +0100

    New pt0, pt1 algorithm, still not finished

--- vision_system/salmon_inference2.py
+++ vision_system/salmon_inference2.py
@@ -96,123 +96,42 @@ def get_scan_start_stop(im, binary_mask):
         # cv2.imwrite(path_results + "contours_" + filename, cv2.resize(temp,(int(width/factor),int(height/factor)), cv2.INTER_LINEAR))     # uncomment to generate images for documentation
 
 
-    rect = cv2.minAreaRect(contours[0])
-    box = cv2.boxPoints(rect)                    
-    box = np.int0(box)
-    # print("Box coordinates ([[x1,y1]...[x4,y4]]) = ", box)
-    # cv2.drawContours(img_draw,[box],0,(0,0,255),2)        # uncomment to generate images for documentation
-    # cv2.imshow("bbox", img_draw)                          # uncomment to generate images for documentation
-    # cv2.imwrite(path_results + "bbox_" + filename, img_draw)      # uncomment to generate images for documentation
+    cnt = contours[0]
+    M = cv2.moments(cnt)
+
+    rows,cols = img.shape[:2]
+    [vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
+
+    pt0 = [x-(50*vx), y-(50*vy)]
+    pt1 = [x+(50*vx), y+(50*vy)]
 
 
-    # Cross: create a cross inside the box.
-    # Average of boxes' coordinates and then converting to int
-    cross = []
-    cross.append( [int(i) for i in  ((box[0]+box[1])/2.0)] )
-    cross.append( [int(i) for i in  ((box[1]+box[2])/2.0)] )
-    cross.append( [int(i) for i in  ((box[2]+box[3])/2.0)] )
-    cross.append( [int(i) for i in  ((box[3]+box[0])/2.0)] )
-    # Draw crosses on the image
-    # cv2.line(img_draw,cross[0], cross[2],(255,255,0),3)       # uncomment to generate images for documentation
-    # cv2.line(img_draw,cross[1], cross[3],(0,255,255),3)       # uncomment to generate images for documentation
-    # cv2.imshow("cross", img_draw)       # uncomment to generate images for documentation
-    # cv2.imwrite(path_results + "cross_" + filename, img_draw)       # uncomment to generate images for documentation
+    inside_check_pt0 = cv2.pointPolygonTest(cnt, (int(pt0[0]), int(pt0[1])), False)
+    inside_check_pt1 = cv2.pointPolygonTest(cnt, (int(pt1[0]), int(pt1[1])), False)
 
-    # Generate a belly line by comparing the l2 norm of the cross's height and width.
-    d1 = np.linalg.norm([cross[0], cross[2]])
-    d2 = np.linalg.norm([cross[1], cross[3]])
-    belly_line = [cross[0], cross[2]] if (d1 >= d2) else [cross[1], cross[3]]    
+    while inside_check_pt0 + inside_check_pt1 != -2: # While both points are inside cnt
+        if inside_check_pt0 >= 0:
+            pt0 = [pt0[0]-(5*vx), pt0[1]-(5*vy)]
+        if inside_check_pt1 >= 0:
+            pt1 = [pt1[0]+(5*vx), pt1[1]+(5*vy)]
+        inside_check_pt0 = cv2.pointPolygonTest(cnt, (int(pt0[0]), int(pt0[1])), False)
+        inside_check_pt1 = cv2.pointPolygonTest(cnt, (int(pt1[0]), int(pt1[1])), False)   
 
-    # Note order of content ! belly_line = [[x1,y1],[x2,y2]]
-    # print("Belly_line[[x1,y1],[x2,y2]] = ", belly_line)    
-    # angle = np.arctan2(y,x), get x and y coordinates.    
-    x = belly_line[0][0] - belly_line[1][0]
-    y = belly_line[0][1] - belly_line[1][1]
-    theta = np.arctan2(y,x)
-    #print("Theta(rads) = ", theta," Theta(deg) = ", theta * 180/np.pi)
+    cv2.circle(img, (int(pt0[0]), int(pt0[1])), 3, (0,0,255),2)
+    cv2.circle(img, (int(pt1[0]), int(pt1[1])), 3, (0,255,0),2)
+    cv2.imshow('lala', img_draw)
 
-    # theta = abs(theta)
+    pt0 = np.array([int(pt0[0]),int(pt0[1])])
+    pt1 = np.array([int(pt1[0]),int(pt1[1])])
 
-    # Use the length of the belly_line to determine the offset
-    belly_norm = np.linalg.norm(belly_line)
-    # print("belly_line length = ", belly_norm)
-    offset = belly_norm/30
-
-    # Create two points that are +90 and -90 to the belly line. 
-    x1,y1 = int(offset * np.cos(theta - np.pi/2)), int(offset * np.sin(theta - np.pi/2))
-    x2,y2 = int(offset * np.cos(theta + np.pi/2)), int(offset * np.sin(theta + np.pi/2))
-
-    sum1 = 0
-    no_of_pts = 100
-    line_1 = np.array([ belly_line[0] + np.array([x1,y1]), belly_line[1] + np.array([x1,y1]) ])
-    line_2 = np.array([ belly_line[0] + np.array([x2,y2]), belly_line[1] + np.array([x2,y2]) ])
-    # Draw the offset lines.
-    # cv2.line(img_draw,line_1[0], line_1[1],(127,127,0),3)       # uncomment to generate images for documentation
-    # cv2.line(img_draw,line_2[0], line_2[1],(0,127,127),3)       # uncomment to generate images for documentation
-    # cv2.imshow("offset lines", img_draw)       # uncomment to generate images for documentation
-    # cv2.imwrite(path_results + "offset_lines_" + filename, img_draw)       # uncomment to generate images for documentation
-
-    line_1_inter = np.linspace(line_1[0], line_1[1], no_of_pts)
-    line_2_inter = np.linspace(line_2[0], line_2[1], no_of_pts)   
     
-    # Cal sum of intensity along line_1 only on R channel of input image.
-    # Note: cv2.line uses the point as [x,y], eg: cv2.line(im,[x1,y1],[x2,y2],color,thickness)
-    # However the image coordinates are flipped ! ie: img[y][x]
-    for p in line_1_inter:
-        # points in the interpolated array are in the form [[x1,y1]...[xn,yn]]
-        # [x,y] need to thresholed to [1920-1,1080-1] else the index will exceed bounds.
-        x = min(int(p[0]), width-1)
-        y = min(int(p[1]), height-1)
-        # x = int(p[0])
-        # y = int(p[1])
-        sum1 = sum1 + img[y,x,2]    # sum = sum + values in Red Channel of img
-    # print("LINE_1 : sum1 = ", sum1)
 
-    sum2 = 0
-    # Cal sum of intensity along line_2 
-    for p in line_2_inter:
-        # points in the interpolated array are [[x1,y1]...[xn,yn]]
-        # [x,y] need to thresholed to [1920-1,1080-1] else the index will exceed bounds.
-        x = min(int(p[0]), width-1)
-        y = min(int(p[1]), height-1)
-        # x = int(p[0])
-        # y = int(p[1])
-        sum2 = sum2 + img[y,x,2]    # sum = sum + values in Red Channel of img
-    # print("LINE_2 : sum2 = ", sum2)
-
-    # # Decide which line to choose as scan line and return 
-    # temp = line_1 if(sum1 >= sum2) else line_2   
-    scan_line = line_1 if(sum1 >= sum2) else line_2
+    lefty = int((-x*vy/vx) + y)
+    righty = int(((cols-x)*vy/vx)+y)
+    cv2.line(img,(cols-1,righty),(0,lefty),(0,255,0),2)
 
 
-
-    # Decide in image space to stop the points from flipping around.
-    # we find max in X between pt0 and pt1, then return the points accordingly, not need to swap.
-    pt0 = np.array([ scan_line[0][0], scan_line[0][1] ])      # [x,y]
-    pt1 = np.array([ scan_line[1][0], scan_line[1][1] ])      # [x,y]  
-    # print("pt0", pt0[0], pt0[1])
-    # print("pt1", pt1[0], pt1[1])
-
-    # Sometimes the calculated scan and belly lines will have -ve coordinates because we use trig functions.
-    # We need to threshold these to the image resolution. 
-    # Opencv automatiically floors or thresholds -ves to 0 and values exceeding the image size are limited to max width or height.
-    pt0 = limit_pt(pt0, width, height)
-    pt1 = limit_pt(pt1, width, height)
-
-
-
-    # A check to stop the flipping of points        
-    if ( pt0[0] < pt1[0] ):
-        cv2.circle(img, (pt0[0],pt0[1]), 2, (0,255,0),5)
-        cv2.circle(img, (pt1[0],pt1[1]), 2, (0,0,255),5)
-        return img, pt0, pt1        
-    elif ( pt1[0] < pt0[0] ):
-        cv2.circle(img, (pt1[0],pt1[1]), 1, (0,255,0),5)
-        cv2.circle(img, (pt0[0],pt0[1]), 1, (0,0,255),5)
-        return img, pt1, pt0        
-    else:
-        print("In get_scan_start_stop: Invalid scan points")
-        return img, [None,None], [None,None]
+    return img, pt0, pt1
     
 #########################################################################
 
@@ -244,8 +163,8 @@ def get_black_spot_coord(im, black_spot_mask):
 
 def get_binary_masks(image):
     print('Trying to get binary mask')
-    belly_mask = cv2.imread(path_to_input+'images_michael/binary_mask_f1.jpg') # Read
-    melanin_mask = cv2.imread(path_to_input+'images_michael/melanin_mask_f1.jpg') # Read
+    belly_mask = cv2.imread(path_to_input+'images_michael/binary_mask_f2.jpg') # Read
+    melanin_mask = cv2.imread(path_to_input+'images_michael/melanin_mask_f2.jpg') # Read
 
     belly_mask = cv2.cvtColor(belly_mask, cv2.COLOR_BGR2GRAY) # Grayscale
     melanin_mask = cv2.cvtColor(melanin_mask, cv2.COLOR_BGR2GRAY) # Grayscale
@@ -263,7 +182,7 @@ def inference(input_source='image'):
 
     if input_source == 'image':
         # Read the image input for further processing
-        raw_image = cv2.imread(path_to_input + 'images_michael/input_f1.jpg')
+        raw_image = cv2.imread(path_to_input + 'images_michael/input_f2.jpg')
         raw_image = cv2.resize(raw_image, (0, 0), fx = 0.5, fy = 0.5)
         im_draw = copy.deepcopy(raw_image)
 
@@ -272,15 +191,14 @@ def inference(input_source='image'):
         if np.any(belly_mask) != None:
 
             im_start_stop, pt0, pt1 = get_scan_start_stop(raw_image, belly_mask)
-            print('p0, p1:',pt0, pt1)
+                  
+            print('type:',type(pt0[0]))
+            print('pt0[0]:',pt0[0])
             
-
             if np.any(melanin_mask) != None:
                 im_black_spot, hull_list, mass_center_list = get_black_spot_coord(raw_image, melanin_mask)
 
-
-
-                cv2.circle(im_black_spot, (int(pt0[0]), int(pt0[1])), 0, (0,0,255),5)
+                cv2.circle(im_black_spot, (pt0[0], pt0[1]), 0, (0,0,255),5)
                 cv2.circle(im_black_spot, (int(pt1[0]), int(pt1[1])), 0, (0,255,0),5)
                 cv2.drawContours(im_black_spot, hull_list, -1, (0,0,255), 1)
                 #cv2.imshow('im_start_stop', im_start_stop)
@@ -293,7 +211,7 @@ def inference(input_source='image'):
 
                 print('path computed, length:', len(path))
 
-                path_segments = path[::10]
+                path_segments = path[0::10]
         
                 
 
